2da813b48c35a4c561e77cb55eedd273f4bd4ce8
Jason Bae <cbae12@umd.edu>
2022-05-04 21:44:44 -0400

    a

--- Final_project.py
+++ Final_project.py
@@ -105,9 +105,10 @@ class Audio_book(Book):
     def rented(self):
         self.overdue_price = super().overdue_price + 5
         print (f"Rented, Renting cost is $5. And this is your overdue price ${self.overdue_price}")
-        
+
+#Functions
 def isOnline():
- 
+    
 def rent_book(books, rented, renting_book):
     if isinstance(renting_book, Book):
         if isinstance(books, list[Book]) & isinstance(rented, list[Book]):
