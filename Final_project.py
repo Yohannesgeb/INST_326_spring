3a08d4dae70b6a6aa09d5e01ef46db576557f678
Jimmy <bobbymick12@gmail.com>
2022-05-04 23:04:55 -0400

    up

--- Final_project.py
+++ Final_project.py
@@ -122,12 +122,22 @@ def isOnline(book):
             #Change the overdue_price = 10
             #Change the due_days = 30
             #Change the overdue_days = 0
+            
+            book.type = "online_book"
+            book.overdue_price = "10"
+            book.due_days = "30"
+            book.overdue_days = "0"
 
         if book.type == "audio_book":
             #Change the type "online audio book"
             #Change the overdue_price = 15
             #Change the due_days = (default value is 60)
             #Change the overdue_days = 0
+            
+            book.type = "online_audio_book"
+            book.overdue_price = "15"
+            book.due_days = "60"
+            book.overdue_days = "0"
 
 def rent_book(books, rented, renting_book):
     if isinstance(renting_book, Book):
