8e7063a7b0087f9f7df69dd1b975ae07b959cc60
Jason Bae <cbae12@umd.edu>
2022-05-05 22:16:08 -0400

    a

--- Final_project.py
+++ Final_project.py
@@ -68,12 +68,6 @@ class Book:
                f"Book title: {self.title}"
                )
         return str
-    
-    def __repr__(self):
-        str = (f"Book({self.book_id},{self.type},{self.title},"
-               f"{self.pub_year},{self.copies},{self.overdue_price},"
-               f"{self.due_days},{self.overdue_days})")
-        return str
 
     def rented(self):
         self.copies += 1
@@ -86,18 +80,15 @@ class Book:
             self.overdue_days = 0
             return True
 
-    def overdue(self):
-        self.due_days = 0
-        self.overdue_price += 5
-
 class Paper_book(Book):
     """Paper Book
 
         Attributes:
     """
-    def __init__(self):
-        type = "paper_book"
-        super().__init__()
+    def __init__(self, book_id, title, pub_year, type = "paper_book", copies = 1, 
+                overdue_price = 0, due_days = -1, overdue_days = 0):
+        super().__init__(book_id, title, pub_year, type, copies, 
+                overdue_price, due_days, overdue_days)
         self.type = type
     
     def __str__(self):
@@ -110,35 +101,19 @@ class Audio_book(Book):
 
         Attributes:
     """
-    def __init__(self):
-        type = "audio_book"
-        super().__init__()
-        self.type = type
+    def __init__(self, book_id, title, pub_year, type = "audio_book", copies = 1, 
+                overdue_price = 0, due_days = -1, overdue_days = 0):
+        super().__init__(book_id, title, pub_year, type, copies, 
+                overdue_price, due_days, overdue_days)
 
     def rented(self):
         self.overdue_price = super().overdue_price + 5
         print (f"Rented, Renting cost is $5. And this is your overdue price ${self.overdue_price}")
     
     def __str__(self):
-        return f"Audio Book ID: {self.book_id} Audio Book title: {self.title}"
-
-#Functions
-def isOnline(book):
-    """Books can be Online format or be Audio book.
-    
-    """
-    if isinstance(book, Book):
-        if book.type == "paper_book":
-            book.type = "online_book"
-            book.overdue_price = "10"
-            book.due_days = "30"
-            book.overdue_days = "0"
-
-        if book.type == "audio_book":
-            book.type = "online_audio_book"
-            book.overdue_price = "15"
-            book.due_days = "60"
-            book.overdue_days = "0"
+        str = (f"Audio Book ID: {self.book_id} \ "
+               f"Audio Book title: {self.title}")
+        return str
 
 def rent_book(books, rented, renting_book):
     if isinstance(renting_book, Book):
@@ -187,11 +162,6 @@ def return_book(books, rented, return_book):
     if changed > 0:
         rented.remove(return_book)
 
-def due_pay(book, pay):
-    if isinstance(book, Book):
-        book.overdue_price -= pay
-    print(f"After paying ${pay}, your due balance is ${book.overdue_price}.")
-
 def main(filepath):
     """The main function
     Recommended tasks:
@@ -212,9 +182,15 @@ def main(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
         for line in f:
             book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days = line.strip().split(',')
+            book_id = int(book_id)
+            pub_year = int(pub_year)
+            copies = int(copies)
+            overdue_price = float(overdue_price)
+            due_days = int(due_days)
+            overdue_days = int(overdue_days)
             book = Book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days)
-            audio = Book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days)
-            paper = Book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days)
+            audio = Audio_book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days)
+            paper = Paper_book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days)
             nr_book = Book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days)
             r_book = Book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days)
             books.append(book)
@@ -227,44 +203,14 @@ def main(filepath):
             if int(due_days) >= 0:
                 r_books.append(r_book)
             print(f"Book ID: {nr_book.book_id} \ Overdue Days: {nr_book.overdue_days}")
-
-            # if book_type == "audio_book":
-            #     a_books.append(Audio_book(book_id, title, pub_year, book_type , copies , overdue_price , due_days , overdue_days))
-    # a_count = 1
-    # for audio in a_books:
-    #     # print(f"Audio Book #{a_count}")
-    #     str = audio.__str__()
-    #     # print(str)
-    #     a_count += 1
-    # p_count = 1
-    # for paper in p_books:
-    #     # print(f"Paper Book #{p_count}")
-    #     str = paper.__str__()
-    #     # print(str)
-    #     p_count += 1
-    # nr_count = 1
-    # for nr_book in nr_books:
-    #     print (f"Not rented book#{nr_count}")
-    #     str = nr_book.__str__()
-    #     print(str)
-    #     nr_count += 1
-    # r_count = 1
-    # for r_book in r_books:
-    #     # print (f"Rented Book #{r_count}")
-    #     str = r_book.__str__()
-    #     # print(str)
-    #     r_count += 1
-    
-    
     while True:
         print ('\n===== Books Menu ======\n'
-               '(1) All books. '
+               '(1) View books. '
                '(2) Rent/Return Books '
-               '(3) Pay Dues '
-               '(4) Exit the program')
+               '(3) Exit the program')
         user = int(input('User input: '))
         if user == 1:
-            print('\n==== All Books ====\n')
+            print('\n==== View Books ====\n')
             count = 1
             for book in books:
                 str = f"Book#{count} {book.__str__()}"
@@ -287,31 +233,21 @@ def main(filepath):
                 user2 = int(input("Enter Book ID: "))
                 for book1 in nr_books:
                     if book1.book_id == user2:
-                        print("YES")
                         rent_book(nr_books, r_books, book1)
                         if book1 in r_books:
                             print(f'You have rented {book1.title}')
-            if user1 == 2:
+            elif user1 == 2:
                 print('\n==== Return ====\n')
                 user2 = int(input("Enter Book ID: "))
                 for book in books:
                     if book.book_id == user2:
                         return_book(nr_books, r_books, book)
+            else:
+                print('Invalid value input. Please try again.')
         elif user == 3:
-            break
-        elif user == 4:
             return
         else:
-            raise ValueError('Invalid value input')
-        #Rent/Return/due pay, etc. with user input. You want to print out
-        #changes that you make or print the entire list?
-
-
-  
-
-    
-
-
+            print('Invalid value input. Please try again.')
 
 if __name__ == "__main__":
     filepath = "books.csv"
